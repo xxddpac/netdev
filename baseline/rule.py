18487098274715f5421f142c20a5586cc870a677
大米 <xxddpac@gmail.com>
2023-04-14 10:22:52 +0800

    feat: add baseline check for cisco_nxos

--- baseline/rule.py
+++ baseline/rule.py
@@ -1,8 +1,10 @@
 """
-根据不同类型设备定义对应的基线扫描项,其中rule字段作为正则匹配判断是否满足基线标准
+根据不同类型设备定义对应的基线扫描项,其中rule字段作为正则匹配判断是否满足基线标准,
+可参考以下格式自行新增删除检测项
 """
 
 scan_template = [
+    # cisco_ios
     {
         'vendor': 'cisco_ios', 'name': 'ntp', 'desc': '开启ntp时间同步协议', 'level': '中危', 'rule': 'ntp server.+'
     },
@@ -32,6 +34,7 @@ scan_template = [
         'vendor': 'cisco_ios', 'name': 'login lock', 'desc': '错误密码尝试登录达到指定次数触发锁定', 'level': '高危',
         'rule': 'login block-for.+'
     },
+    # huawei
     {
         'vendor': 'huawei', 'name': 'ntp', 'desc': '开启ntp时间同步协议', 'level': '中危', 'rule': 'ntp.*? unicast-server.+'
     },
@@ -60,5 +63,35 @@ scan_template = [
     {
         'vendor': 'huawei', 'name': 'login lock', 'desc': '错误密码尝试登录达到指定次数触发锁定', 'level': '高危',
         'rule': 'ssh server authentication-retries.+'
-    }
+    },
+    # cisco_nxos
+    {
+        'vendor': 'cisco_nxos', 'name': 'ntp', 'desc': '开启ntp时间同步协议', 'level': '中危', 'rule': 'ntp server.+'
+    },
+    {
+        'vendor': 'cisco_nxos', 'name': 'syslog', 'desc': '开启syslog日志功能并记录至服务器', 'level': '中危',
+        'rule': 'logging server.+'
+    },
+    {
+        'vendor': 'cisco_nxos', 'name': 'aaa', 'desc': '开启3A认证审计', 'level': '高危', 'rule': 'aaa group server tacacs\\+.+'
+    },
+    {
+        'vendor': 'cisco_nxos', 'name': 'ssh', 'desc': '开启SSH白名单限制指定客户端登录', 'level': '高危', 'rule': 'access-class.*?in'
+    },
+    {
+        'vendor': 'cisco_nxos', 'name': 'snmp', 'desc': '开启网管协议snmp', 'level': '中危',
+        'rule': 'snmp-server community.*?group.+'
+    },
+    {
+        'vendor': 'cisco_nxos', 'name': 'telnet', 'desc': '禁止开启telnet服务', 'level': '严重',
+        'rule': 'feature telnet'
+    },
+    {
+        'vendor': 'cisco_nxos', 'name': 'login timeout', 'desc': 'SSH登录在超时时间内无操作将自动登出', 'level': '高危',
+        'rule': 'exec-timeout.+'
+    },
+    {
+        'vendor': 'cisco_nxos', 'name': 'login lock', 'desc': '错误密码尝试登录达到指定次数触发锁定', 'level': '高危',
+        'rule': 'ssh login-attempts.+'
+    },
 ]
